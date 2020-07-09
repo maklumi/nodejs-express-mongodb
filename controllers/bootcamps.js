63524143cf8589195d8371a0ca2e97505eb8b85d
kfc@dell <kfc@dell.com>
2020-07-10 06:53:35 +0800

    Update and Delete a bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,9 @@ const Bootcamp = require('../models/Bootcamp')
 exports.dapatkanSemuaBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find()
-    res.status(200).json({ berjaya: true, data: bootcamps })
+    res
+      .status(200)
+      .json({ berjaya: true, bilangan: bootcamps.length, data: bootcamps })
   } catch (err) {
     res.status(400).json({
       berjaya: false,
@@ -52,17 +54,44 @@ exports.ciptaBootcamp = async (req, res, next) => {
 // @desc    Update satu bootcamp dengan id ini
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ berjaya: true, mesej: `Update bootcamps id ${req.params.id}` })
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!bootcamp) {
+      return res.status(400).json({
+        berjaya: false,
+        mesej: `Tiada bootcamp dengan id ${req.params.id}`,
+      })
+    }
+    res.status(200).json({ berjaya: true, data: bootcamp })
+  } catch (err) {
+    res.status(400).json({
+      berjaya: false,
+      mesej: 'Gagal dapatkan bootcamp' + err.message,
+    })
+  }
 }
 
 // @desc    Delete satu bootcamp dengan id ini
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ berjaya: true, mesej: `Padam bootcamps id ${req.params.id}` })
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+    if (!bootcamp) {
+      return res.status(400).json({
+        berjaya: false,
+        mesej: `Tiada bootcamp dengan id ${req.params.id}`,
+      })
+    }
+    res.status(200).json({ berjaya: true, data: {} })
+  } catch (err) {
+    res.status(400).json({
+      berjaya: false,
+      mesej: 'Gagal padam bootcamp ' + err.message,
+    })
+  }
 }

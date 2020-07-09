f3bcc55d0ef3df430d73e130ffa9ed0f15e8f789
kfc@dell <kfc@dell.com>
2020-07-09 15:35:55 +0800

    log message dengan morgan middleware

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config/config.env' })
+// const logger = require('./middeware/logger')
+const morgan = require('morgan')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 const bootcampRoutes = require('./routes/bootcamps')
+// app.use(logger)
+app.use(morgan('dev'))
+
 app.use('/api/v1/bootcamps', bootcampRoutes)
 
 app.listen(

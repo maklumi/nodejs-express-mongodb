46706b8cf9b5ffec22bb51450bf4ea54056dfcb9
kfc@dell <kfc@dell.com>
2020-07-10 15:59:53 +0800

    database seeder

---
+++ dbseeder.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+// load env variables dulu
+dotenv.config({ path: './config/config.env' })
+
+// load models
+const Bootcamp = require('./models/Bootcamp')
+
+// connect to db
+mongoose.connect(process.env.URI_MONGO, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+// baca json files
+const docs = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'),
+)
+
+// masukkan dalam db
+const masukdata = async () => {
+  try {
+    await Bootcamp.create(docs)
+    console.log('Data sudah dimasukkan...'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(err)
+  }
+}
+
+// padam apa dalam db
+const padamdata = async () => {
+  try {
+    await Bootcamp.deleteMany()
+    console.log('Data sudah dipadam'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(err)
+  }
+}
+
+// prompt$ node dbseeder [-i|-d] untuk fungsi di bawah
+if (process.argv[2] === '-i') {
+  masukdata()
+} else if (process.argv[2] === '-d') {
+  padamdata()
+}
